67c0aa5897f65d3c55dd13255664c81174c251b1
zhangzhongyao <zhongyaozhang218@163.com>
2024-04-04 20:49:49 +0800

    更新sha-acc；修改sha2配置文件

--- generators/chipyard/src/main/scala/config/RocketSha2Configs.scala
+++ generators/chipyard/src/main/scala/config/RocketSha2Configs.scala
@@ -5,24 +5,16 @@ import freechips.rocketchip.diplomacy.{AsynchronousCrossing}
 
 // --------------
 // Rocket+SHA2 Configs
-// These live in a separate file to simplify patching out for the tutorials.
 // --------------
 
-// DOC include start: Sha2Rocket
-// class Sha2RocketConfig extends Config(
-//   new sha2.WithSha2Accel ++                                // add SHA2 rocc accelerator
-//   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-//   new chipyard.config.AbstractConfig)
-// DOC include end: Sha2Rocket
+class Sha2RocketConfig extends Config(
+  new sha2.WithSha2Accel ++                                // add SHA2 rocc accelerator
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
 
-// class Sha2RocketPrintfConfig extends Config(
-//   new sha2.WithSha2Printf ++
-//   new sha2.WithSha2Accel ++                                // add SHA2 rocc accelerator
-//   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-//   new chipyard.config.AbstractConfig)
+class Sha2RocketPrintfConfig extends Config(
+  new sha2.WithSha2Printf ++
+  new sha2.WithSha2Accel ++                                // add SHA2 rocc accelerator
+  new freechips.rocketchip.subsystem.WithNBigCores(1) ++
+  new chipyard.config.AbstractConfig)
 
-// class Sha2BBRocketConfig extends Config(
-//   new sha2.WithSha2BlackBox ++
-//   new sha2.WithSha2Accel ++                                // add SHA2 rocc accelerator
-//   new freechips.rocketchip.subsystem.WithNBigCores(1) ++
-//   new chipyard.config.AbstractConfig)
